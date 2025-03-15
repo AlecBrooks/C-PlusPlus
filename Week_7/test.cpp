d6d7b64ca502f81f4e49028a60ec085e0866b6c0
Alec Brooks <alec@abrooks.dev>
2025-03-15 04:52:22 -0700

    123

--- Week_7/test.cpp
+++ Week_7/test.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 
+int addTwo(int,int);
+
+
 int main(){
 
 	int x;
+	int y;
+
 	cout << "Enter an Int: ";
 	cin >> x;
-	cout << "you typed: " << x << endl;
-
+	cout << endl << "Enter another int: ";
+	cin >> y;
+	cout << endl << x << " + " << y << " = " << addTwo(x,y) << endl;
 	return 0;
+
+}
+
+int addTwo(int x,int y){
+
+	return x + y;
 
 }
