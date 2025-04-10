99d53f6eabf029c606eebb3536f14fefeed41f4d
Alec Brooks <alec@abrooks.dev>
2025-04-10 06:10:21 -0700

    working hard

--- Project/game.cpp
+++ Project/game.cpp
@@ -2,27 +2,57 @@
 #include <string>
 #include <random>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m" // bad values
+#define GREEN   "\033[32m" // good values
+#define YELLOW  "\033[33m" // enemy creatures
+#define BLUE    "\033[34m" // friendly creatures
+#define MAGENTA "\033[35m" // items
+#define CYAN    "\033[36m" // weapons
 
 //startGlobal RNG engine
 random_device rd;
 mt19937 rng(rd());
 
 //prototypes
-void runRoom();
-void runCombat();
+class Hero;
+class Item;
+class Monster;
+class Room;
 int runRNG(int min, int max);
+void clearScreen();
+void runRoom();
+void showHeader(Hero&);
+void runCombat(Hero&, Monster&);
+void createObjects();
+void useItemLogic(Hero&, Item&);
 
-//Classes
+
+//Classes====================================
 
 //create items (Class)
 class Item {
 	public:
     	string name;
     	string desc;
-    Item(string name, string desc)
-        : name(name), desc(desc) {}
+    	bool singleUse;
+    	bool canCarry;
+
+    Item(string name, string desc, bool singleUse, bool canCarry)
+        : name(name), desc(desc), singleUse(singleUse), canCarry(canCarry){}
+
+	void useMe(Hero& player) {
+		useItemLogic(player, *this);
+	};
+
+    void useOn(Item& Target){
+
+    };
+
 };
 
 //create weapon (Class)
@@ -32,12 +62,12 @@ class Weapon{
 		int minDmg;
 		int maxDmg;
 
-	Weapon(string name, int minDmg, int maxDmg, string desc)
+	Weapon(string name, int minDmg, int maxDmg)
 		: name(name), minDmg(minDmg), maxDmg(maxDmg) {}
 
 	int swing() {
 		int dmg = runRNG(minDmg,maxDmg);
-		cout << name << " deals " << dmg << " damage!" << endl;
+		cout << CYAN << name << RESET << " dealing " << RED << dmg << RESET << " damage!" << endl;
 		return dmg;
 	};
 };
@@ -46,15 +76,17 @@ class Weapon{
 class Hero{
 	public:
 	string name;
+	int maxHP;
 	int hp;
 	int st;
 	int dex;
 	int armorClass;
 	Weapon* weapon;
 	vector<Item> inv;
+	Room* room;
 
-	Hero(string name, int hp, int st, int dex, Weapon* weapon)
-		: name(name), hp(hp), st(st), dex(dex), armorClass(10+dex){}
+	Hero(string name, int maxHP, int hp, int st, int dex, Weapon* weapon, Room* room)
+    : name(name), maxHP(maxHP), hp(hp), st(st), dex(dex), armorClass(10 + dex), weapon(weapon), room(room){}
 
 	int attackRoll(int targetAC){
 
@@ -63,7 +95,7 @@ class Hero{
 		int attackTotal = roll + bonus;
 
 		if(attackTotal >= targetAC){
-        	cout << "You hit!" << endl;
+        	cout << "You hit with your ";
         	return weapon->swing();
 		}else{
         	cout << "You missed!" << endl;
@@ -77,44 +109,70 @@ class Hero{
 		return hp;
 	};
 
+	int heal(int healAmount){
+		hp += healAmount;
+		if (hp > maxHP) hp = maxHP;
+		return hp;
+	};
+
 	void showStats(){
-		cout << "Name: " << name << endl;
-		cout << "Health: " << hp << endl;
-		cout << "Weapon: " << weapon -> name << endl;
-		cout << "Inventory:" << endl;
+		cout << "Name: " << CYAN << name << RESET << endl;
+		cout << "Health: " << GREEN << hp << RESET << endl;
+
+		if (weapon != nullptr) {
+			cout << "Weapon: " << weapon->name << endl;
+		} else {
+			cout << "Weapon: (none)" << endl;
+		};
+
+		cout << YELLOW <<"Inventory:" << endl;
 		if(inv.empty()){
-			cout << "(empty)" << endl; 
+			cout << "(empty)" << RESET << endl; 
 		}else{
 			for (int i = 0; i < inv.size(); ++i) {
 				cout << " - " << inv[i].name << endl;
-			}
-		}
+			};
+			cout << RESET;
+		};
 	};
 
 	void addItem(Item item) {
-    	inv.push_back(item);
-    	cout << name << " picked up a: " << item.name << endl;
+		if(item.canCarry == true){
+    		inv.push_back(item);
+    		cout << name << " picked up a: " << CYAN << item.name << RESET << endl;
+    	} else {
+    		cout << "You're kidding right?" << endl;
+    	};
 	};
 
 
 	void equipWeapon(Weapon* newWeapon) {
     	weapon = newWeapon;
-    	cout << name << " equipped a: " << newWeapon->name << endl;
+    	cout << name << " equipped a: " << CYAN << newWeapon->name << RESET << endl;
 	};
 
 
 	void examine(string itemName) {
     	for (int i = 0; i < inv.size(); ++i) {
         	if (inv[i].name == itemName) {
-            	cout << inv[i].name << ": " << inv[i].desc << endl;
+            	cout << CYAN << inv[i].name << RESET << ": " << inv[i].desc << endl;
             	return;
-        	}
-    	}
-    	cout << itemName << " you dont have that." << endl;
+        	};
+    	};
+    	cout << CYAN << itemName << RESET << " you dont have that." << endl;
 	};
 
-	void useItem(Item item){
-	
+	void useItem(string itemName) {
+		for (int i = 0; i < inv.size(); ++i) {
+			if (inv[i].name == itemName) {
+				inv[i].useMe(*this);
+				if (inv[i].singleUse) {
+					inv.erase(inv.begin() + i);
+				};
+			return;
+			};
+		};
+		cout << CYAN << itemName << RESET << " is not in your inventory!" << endl;
 	};
 
 	bool isAlive() {
@@ -123,7 +181,7 @@ class Hero{
 
 };
 	
-	//create Monsters (Class)
+//create Monsters (Class)
 class Monster{
 	public:
 	string name;
@@ -133,9 +191,10 @@ class Monster{
 	int armorClass;
 	Weapon* weapon;
 	string desc;
+	string deathDesc;
 
-	Monster(string name, int hp, int st, int dex, Weapon* weapon, string desc)
-		: name(name), hp(hp), st(st), dex(dex), armorClass(10+dex), desc(desc){}
+	Monster(string name, int hp, int st, int dex, Weapon* weapon, string desc, string deathDesc)
+		: name(name), hp(hp), st(st), dex(dex), armorClass(10+dex), weapon(weapon), desc(desc), deathDesc(deathDesc){}
 
 	int attackRoll(int targetAC){
 
@@ -144,10 +203,10 @@ class Monster{
 		int attackTotal = roll + bonus;
 
 		if(attackTotal >= targetAC){
-        	cout << "the " << name << " hit!" << endl;
+        	cout << "The " << YELLOW << name << RESET << " hit with it's ";
         	return weapon->swing();
 		}else{
-        	cout << "the " << name << " Missed!" << endl;
+        	cout << "The " << YELLOW << name << RESET << " Missed!" << endl;
         	return 0; 
 		};
 	};
@@ -162,26 +221,61 @@ class Monster{
 		return hp;
 	};
 
+	void showStats(){
+		cout << "Name: " << YELLOW << name << RESET << endl;
+		cout << "Health: " << GREEN << hp << RESET << endl;
+
+		if (weapon != nullptr) {
+			cout << "Weapon: " << weapon->name << endl;
+		} else {
+			cout << "Weapon: (none)" << endl;
+		};
+	};
+
 };
 
-	//create rooms (Class)
+//create rooms (Class)
+class Room{
+	public:
+	string name;
 
+	Room(string name)
+	: name(name) {}
+
+};
 
 //main loop and init
 int main(){
+	//Create Objects
 
-	bool gameOver = false;
-	int gameOverType = 0; 
+	//rooms
+	Room testRoom("testRoom");
 
-	while(gameOver == false){
-		//game Loop	
+	//items
+	Item hpot("health potion", "Can be used to heal 5 - 10 health.",true,true);
+	Item ppot("poison potion", "Ingestion will cause 5 - 10 damage.",true,true);
+	Item map("map", "Map of the Area",false,true);
+	Item BoS("boulder of sisyphus", "A fancy way of saying a big ass rock.",false,false);
 
-	}
-	if(gameOverType == 0){
-		//player loss to death
-	}else{
-		//player win 
-	} 
+
+	//weapons
+	Weapon starterSword("Rusty Sword", 2, 5);
+	Weapon claw("Bloodly Claws", 2, 3);
+	
+	//monsters
+	Monster goblin("Goblin", 10, 1, 1, &claw, "A small, green creature with bloodstained claws and a cruel grin.", "The goblin screeches as your blow lands true. Blood sprays from its mouth as it stumbles backward, eyes wide in shock. With a final gasp, it crumples to the ground, twitching once before going still.");
+
+	//player
+	Hero player("Aragorn", 30, 30, 3, 2, &starterSword, &testRoom);
+
+	player.addItem(hpot);
+	player.addItem(hpot);
+	player.addItem(ppot);
+	player.addItem(map);
+	player.addItem(BoS);
+
+	clearScreen();
+	runCombat(player,goblin);
 
 	return 0;
 }
@@ -195,6 +289,93 @@ void runRoom(){
 
 };
 
-void runCombat(){
-
+void clearScreen(){
+	cout << "\033[2J\033[1;1H"; 
 };
+
+void Header(Hero& player, Room& ThisRoom) {
+    cout << "===== Round " << ThisRoom.name << " =====" << endl << endl;
+    cout << BLUE << "-- Player --" << RESET << endl;
+    player.showStats();
+    cout << "=====================" << endl << endl;
+}
+
+void CombatHeader(Hero& player, Monster& enemy, int combatRound) {
+    cout << "===== Round " << combatRound << " =====" << endl << endl;
+    cout << BLUE << "-- Player --" << RESET << endl;
+    player.showStats();
+    cout << endl << RED << "-- Enemy --" << RESET << endl;
+    enemy.showStats();
+    cout << endl << "What will you do?" << endl;
+    cout << "> attack | flee | use [item name]" << endl << endl;
+    cout << "=====================" << endl << endl;
+}
+
+void runCombat(Hero& player, Monster& enemy){
+	int combatRound = 1;
+	while (player.isAlive() && enemy.isAlive()){
+		CombatHeader(player, enemy, combatRound);
+
+		string action;
+		getline(cin,action);
+		cout << endl;
+		if (action == "attack") {
+    		int playerDmg = player.attackRoll(enemy.armorClass);
+    		if (playerDmg > 0) {
+        		enemy.takeDamage(playerDmg);
+    		}
+    		int enemyDmg = enemy.attackRoll(player.armorClass);
+    		if (enemyDmg > 0) {
+        		player.takeDamage(enemyDmg);
+    		}
+    		combatRound += 1;
+		} else if (action == "flee") {
+    		cout << "You turn to flee giving the " << YELLOW << enemy.name << RESET << " an opertunity to attack! " << endl;
+    		int enemyDmg = enemy.attackRoll(player.armorClass);
+    		if (enemyDmg > 0) {
+        		player.takeDamage(enemyDmg);
+    		}
+    		break;
+    	}else if (action.find("use ") == 0){
+    		string itemName = action.substr(4);
+    		player.useItem(itemName);
+		} else {
+    		cout << "no acction for this command." << endl;
+		}
+		cin.get();
+		clearScreen();				
+	};
+	if(!enemy.isAlive()){
+		CombatHeader(player, enemy, combatRound);
+		cout << enemy.deathDesc << endl;
+	}else if(!player.isAlive()){
+		CombatHeader(player, enemy, combatRound);
+		cout << "you lose!";
+	}else{
+
+	}
+	cin.get();
+};
+
+void useItemLogic(Hero& player, Item& item){
+
+	if (item.name == "health potion") {
+    	int healAmount = runRNG(5, 10); 
+        cout << "You use the " << MAGENTA << item.name << RESET
+        << " and restore " << GREEN << healAmount << RESET << " health." << endl;
+        player.heal(healAmount);
+    }
+    if(item.name == "poison potion") {
+    	int damage = runRNG(5,10);
+    	cout << "You use the " << MAGENTA << item.name << RESET
+        << " and suffer " << GREEN << damage << RESET << " damage." << endl;
+        player.takeDamage(damage);
+    };
+    if(item.name == "map"){
+    	cout << "You look at the " << MAGENTA << item.name << RESET << ", nice pictures." << endl;
+    };
+};
+
+
+
+
